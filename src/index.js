7a347c017f85cdfb8f39cd3101b5b927b3fddc18
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-04-01 13:47:17 +0800

    Add Custom Elements

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './script/component/nav-bar.js'
+import './script/component/quote-card.js'
+import './script/component/social-media.js'
+import './script/component/copy-right.js'
+// import main from './script/main.js'
+
+// document.addEventListener('DOMContentLoaded', main)
