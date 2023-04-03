2deadb0be0d7eb42ff21412aa3cf8b2984cbf250
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-04-03 10:37:46 +0800

    Fetching data with Axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/main.css'
 import './script/components/nav-bar.js'
-import './script/components/quote-card.js'
+// import './script/components/quote-card.js'
 import './script/components/social-media.js'
 import './script/components/copy-right.js'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
